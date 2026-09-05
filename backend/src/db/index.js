32bdd467f7b3b6232037f51ebab3d833891c92b5
Dhanesh635 <dhaneshsharma635@gmail.com>
2026-09-05 10:18:19 +0530

    first

---
+++ backend/src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI)
+        console.log(`MongoDB connected successfully DB_HOST: ${connectionInstance.connection.host}`);
+    
+    }catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import {app} from './app.js';
+import connectDB from './db/index.js';
+import dotenv from 'dotenv';
+dotenv.config({
+    path: './.env' 
+});
+
+const PORT = process.env.PORT || 4000;
+
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log('Server is running on port', PORT);
+    });
+})
+.catch((error) => {
+    console.error('Error connecting to the database:', error);
+    process.exit(1);
+});
+
