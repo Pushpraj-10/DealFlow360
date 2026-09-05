17f7d776da604c85eae46a468b885d62ceda21d0
Pushpraj-10 <pushpraj.nareti@gmail.com>
2026-09-05 10:41:10 +0530

    Changed folder structure

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ app.use(express.json({limit: '16kb'}));
 app.use(express.urlencoded({limit: '16kb', extended: true}));
 app.use(express.static('public'));
 
-import healthcheckRoutes from './routes/healthcheck.routes.js';
+import healthcheckRoutes from './modules/healthcheck/healthcheck.routes.js';
 
 app.use('/api/v1/healthcheck', healthcheckRoutes);
 
--- backend/src/modules/healthcheck/healthcheck.controller.js
+++ backend/src/modules/healthcheck/healthcheck.controller.js
@@ -1,5 +1,5 @@
-import {ApiResponse} from '../utils/apiResponse.js';
-import {asyncHandler} from '../utils/asyncHandler.js';
+import {ApiResponse} from '../../core/utils/apiResponse.js';
+import {asyncHandler} from '../../core/utils/asyncHandler.js';
 
 const healthCheck = asyncHandler(async (req, res) => {
     return res
